f7c1067661b0c72649f08471e23de636845f3ef5
suhaib khazaleh <114153249+suha1b-kh@users.noreply.github.com>
2024-10-18 23:15:07 +0300

    finish add report

--- lib/student/profile/screens/add_report_screen.dart
+++ lib/student/profile/screens/add_report_screen.dart
@@ -6,7 +6,9 @@ import 'package:diente/core/widgets/buttons.dart';
 import 'package:diente/core/widgets/text.dart';
 import 'package:diente/patient/presentation/widgets/custom_text_field.dart';
 import 'package:diente/student/data/models/report_model.dart';
+import 'package:diente/student/data/services/choose_case.dart';
 import 'package:diente/student/data/services/reports_services.dart';
+import 'package:diente/student/profile/screens/control_screen.dart';
 import 'package:diente/student/profile/widgets/student_appbar.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
@@ -50,63 +52,81 @@ class _AddReportScreenState extends State<AddReportScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _imageFile == null
-            ? Image.asset('assets/images/p.png')
-            : Image.file(
-                height: 150.w,
-                _imageFile!.absolute,
-                fit: BoxFit.cover,
-              ),
-        Gap(35.h),
-        customText(context, 'Add photo of your report',
-            Theme.of(context).colorScheme.secondary, 14.sp, FontWeight.w400),
-        Gap(35.h),
-        CustomTextField(
-          hint: 'add description',
-          textAlign: TextAlign.center,
-          inputType: TextInputType.text,
-          obscureText: false,
-        ),
-        Gap(20.h),
-        customDialogButton(context, Theme.of(context).colorScheme.secondary,
-            () async {
-          await _pickImage(ImageSource.gallery);
+        body: Padding(
+      padding: EdgeInsets.only(top: 50.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'please add photo for your report to complete finish the case',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 24.sp,
+              fontWeight: FontWeight.w400,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Gap(50.w),
+          _imageFile == null
+              ? Image.asset('assets/images/p.png')
+              : Image.file(
+                  height: 150.w,
+                  _imageFile!.absolute,
+                  fit: BoxFit.cover,
+                ),
+          Gap(35.h),
+          // CustomTextField(
+          //   hint: 'add description',
+          //   textAlign: TextAlign.center,
+          //   inputType: TextInputType.text,
+          //   obscureText: false,
+          // ),
+          Gap(20.h),
+          customDialogButton(context, Theme.of(context).colorScheme.secondary,
+              () async {
+            await _pickImage(ImageSource.gallery);
 
-          if (_imageFile != null) {
-            //reference to the firebase storage
-            final storageRef = FirebaseStorage.instance.ref();
-            //upload the image to the firebase storage
-            UploadTask task = storageRef
-                .child('reports_pic/${_imageFile!.path.split('/').last}')
-                .putFile(_imageFile!);
-            //get the download url of the image
-            task.whenComplete(() async {
-              final downloadUrl = storageRef
-                  .child('reports_pic/${_imageFile!.path.split('/').last}');
-              downloadUrlString = await downloadUrl.getDownloadURL();
-              log(downloadUrlString.toString());
-            });
-          }
-          log('message');
-          addReport(
-            ReportModel(
-              caseName: widget.caseName,
-              reportPic: downloadUrlString!,
-              reportId: widget.caseId,
-            ),
-          );
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: const Text('Report added successfully!'),
-              backgroundColor: Theme.of(context).colorScheme.secondary,
-              duration: const Duration(seconds: 2),
-            ),
-          );
-        }, 'select photo', 16.sp),
-      ],
+            if (_imageFile != null) {
+              //reference to the firebase storage
+              final storageRef = FirebaseStorage.instance.ref();
+              //upload the image to the firebase storage
+              UploadTask task = storageRef
+                  .child('reports_pic/${_imageFile!.path.split('/').last}')
+                  .putFile(_imageFile!);
+              //get the download url of the image
+              task.whenComplete(() async {
+                final downloadUrl = storageRef
+                    .child('reports_pic/${_imageFile!.path.split('/').last}');
+                downloadUrlString = await downloadUrl.getDownloadURL();
+                log(downloadUrlString.toString());
+              });
+            }
+            log('message');
+            addReport(
+              ReportModel(
+                caseName: widget.caseName,
+                reportPic: downloadUrlString!,
+                reportId: widget.caseId,
+              ),
+            );
+
+            finishCase(widget.caseId);
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: const Text('Report added successfully!'),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                duration: const Duration(seconds: 1),
+              ),
+            );
+            await Future.delayed(const Duration(seconds: 2));
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const ControlScreen()),
+                (route) => false);
+          }, 'select photo', 16.sp),
+        ],
+      ),
     ));
   }
 }
